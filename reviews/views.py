366bc5e4ecd7535b660a86a89b04fc6d7dad7a38
iblug <1115jkb@gmail.com>
2023-04-28 16:05:24 +0900

    리뷰 상세, 리뷰 삭제, 리뷰 수정, 댓글 생성, 리뷰 좋아요

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from restaurants.models import Restaurant
 from .models import Review, Comment
-from .forms import ReviewForm
+from .forms import ReviewForm, CommentForm
 
 def create(request, restaurant_id):
     if request.method == 'POST':
@@ -24,16 +24,23 @@ def create(request, restaurant_id):
     
 def detail(request, restaurant_id, review_id):
     review = Review.objects.get(pk=review_id)
+    comment_form = CommentForm()
+    comments = Comment.objects.filter(review_id=review_id)
+    print(comments)
     context = {
-        'review': review
-        }
+        'review': review,
+        'comment_form': comment_form,
+        'restaurant_id': restaurant_id,
+        'review_id': review_id,
+        'comments': comments,
+    }
     return render(request, 'reviews/detail.html', context)
     
     
 def delete(request, restaurant_id, review_id):
-
-    # POST
-    return render(request, 'reviews/delete.html',)
+    review = Review.objects.get(pk=review_id)
+    review.delete()
+    return redirect('restaurants:detail', restaurant_id=restaurant_id)
     
     
 def update(request, restaurant_id, review_id):
@@ -44,7 +51,7 @@ def update(request, restaurant_id, review_id):
             review = review_form.save(commit=False)
             review.user = request.user
             review.save()
-            return redirect('reviews:detail', restaurant_id=restaurant_id, review_id=review.id)
+            return redirect('reviews:detail', restaurant_id=restaurant_id, review_id=review_id)
     else:
         form = ReviewForm(instance=review)
     context = {
@@ -57,13 +64,28 @@ def update(request, restaurant_id, review_id):
     
     
 def likes(request, restaurant_id, review_id):
-    # POST
-    return render(request, 'reviews/likes.html',)
-
+    review = Review.objects.get(pk=review_id)
+    if review.like_users.filter(pk=request.user.pk).exists():
+        review.like_users.remove(request.user)
+    else:
+        review.like_users.add(request.user)
+    return redirect('reviews:detail', restaurant_id=restaurant_id, review_id=review_id)
 
 def comment_create(request, restaurant_id, review_id):
-    # POST
-    return render(request, 'reviews/comment_create.html')
+    restaurant = Restaurant.objects.get(pk=restaurant_id)
+    review = Review.objects.get(pk=review_id)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.user = request.user
+        comment.review = review
+        comment.restaurant = restaurant
+        comment.save()
+        return redirect('reviews:detail', restaurant_id=restaurant_id, review_id=review_id)
+    context = {
+        'comment_form': comment_form,
+    }
+    return redirect('reviews:detail', restaurant_id=restaurant_id, review_id=review_id, context=context)
 
 
 def comment_delete(request, restaurant_id, review_id, comment_id):
