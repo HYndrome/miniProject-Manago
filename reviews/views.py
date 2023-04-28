a58b02ebe4ec1e6060efdda9846b457e4896f231
iblug <1115jkb@gmail.com>
2023-04-28 10:51:08 +0900

    admin 등록, url 라우팅, view 함수 기초

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def create(request, restaurant_id):
+    pass
+def detail(request, restaurant_id, review_id):
+    pass
+def delete(request, restaurant_id, review_id):
+    pass
+def update(request, restaurant_id, review_id):
+    pass
+def likes(request, restaurant_id, review_id):
+    pass
+def comment_create(request, restaurant_id, review_id):
+    pass
+def comment_delete(request, restaurant_id, review_id, comment_id):
+    pass
+
