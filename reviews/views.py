ef347f7781eac035dbe299398f87ce82e08a45a5
iblug <1115jkb@gmail.com>
2023-04-28 14:05:52 +0900

    render html

--- reviews/views.py
+++ reviews/views.py
@@ -6,21 +6,39 @@ from .forms import ReviewForm
 def create(request, restaurant_id):
     restaurant = Restaurant.objects.get(pk=restaurant_id)
     review_form = ReviewForm(request.POST)
-    pass
+    context = {
+
+    }
+    return render(request, 'reviews/create.html', context)
+    
+    
 def detail(request, restaurant_id, review_id):
     review = Review.objects.get(pk=review_id)
     context = {
         'review': review
         }
     return render(request, 'reviews/detail.html', context)
+    
+    
 def delete(request, restaurant_id, review_id):
-    pass
+    # POST
+    return render(request, 'reviews/delete.html',)
+    
+    
 def update(request, restaurant_id, review_id):
-    pass
+    return render(request, 'reviews/update.html',)
+    
+    
 def likes(request, restaurant_id, review_id):
-    pass
-def comment_create(request, restaurant_id, review_id):
-    pass
-def comment_delete(request, restaurant_id, review_id, comment_id):
-    pass
+    # POST
+    return render(request, 'reviews/likes.html',)
 
+
+def comment_create(request, restaurant_id, review_id):
+    # POST
+    return render(request, 'reviews/comment_create.html')
+
+
+def comment_delete(request, restaurant_id, review_id, comment_id):
+    # POST
+    return render(request, 'reviews/comment_delete.html')
