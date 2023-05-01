1532474fbdc6a7b40d25a9ab840240e910e19e22
“SSONGNOSS66” <ssongnoss66@gmail.com>
2023-05-01 20:03:54 +0900

    SORTING ING

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Restaurant, Menu
+from reviews.models import Review
 from reviews.forms import Review
 from .forms import RestaurantForm, MenuForm
 from reviews.forms import ReviewForm
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.db.models import Avg
 
 # Create your views here.
 
@@ -94,9 +96,11 @@ def wish(request, restaurant_id):
 
 def category(request, restaurant_category):
     category_restaurants = Restaurant.objects.filter(category=restaurant_category)
+    reviews = Review.objects.all()
     context = {
             'restaurant_category': restaurant_category,
             'category_restaurants': category_restaurants,
+            'reviews': reviews,
         }
     return render(request, 'restaurants/category.html', context)
 
