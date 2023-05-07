a11fc99f1ba352c9d8c4d315fde8a444d642814a
jangjaeyong1115 <leoangella12@naver.com>
2023-05-07 15:34:20 +0900

    profile

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,16 @@ from imagekit.models import ProcessedImageField
 from django.db import models
 from imagekit.processors import Thumbnail
 
-
+CHOICES_REGION = [
+    ("서울", "서울특별시"),
+    ("경기도", "경기도"),
+    ("인천", "인천광역시"),
+    ("강원도", "강원도"),
+    ("전라북도", "전라북도"),
+    ("전라남도", "전라남도"),
+    ("경상북도", "경상북도"),
+    ("경상남도", "경상남도"),
+]
 
 
 # Create your models here.
@@ -20,3 +29,4 @@ class User(AbstractUser):
         options= {'quality':90},
         upload_to = '',
     )
+    region = models.CharField(max_length=10, choices=CHOICES_REGION)
