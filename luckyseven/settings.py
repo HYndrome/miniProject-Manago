9ff27b89f2b55efe891d9f24efca23b1fce64861
HYndrome <hyndrome@gmail.com>
2023-05-07 23:17:55 +0900

    update channels layers for redis

--- luckyseven/settings.py
+++ luckyseven/settings.py
@@ -152,7 +152,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('34.64.184.142', 6379)],
         },
     },
 }
